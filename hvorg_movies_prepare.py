a375bed7ae7bdc7f076510eb4774fb4115c15e3a
Jack Ireland <jackireland@gmail.com>
2017-11-15 13:54:35 -0500

    better data frames for usage

--- hvorg_movies_prepare.py
+++ hvorg_movies_prepare.py
@@ -4,14 +4,16 @@
 
 import os
 import datetime
+import pickle
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import astropy.units as u
 from sunpy.time import parse_time
 
+# Save the data
+save_directory = os.path.expanduser('~/Data/hvanalysis/derived')
+
 # Read in the data
-directory = '~/Data/hvanalysis'
+directory = os.path.expanduser('~/Data/hvanalysis/source')
 hvorg_movies = 'movies.csv'
 path = os.path.expanduser(os.path.join(directory, hvorg_movies))
 df = pd.read_csv(path)
@@ -42,16 +44,69 @@ movie_mid_point = [movie_start_time[i] + datetime.timedelta(seconds=0.5*movie_du
 # can
 time_difference = np.asarray([(request_time[i] - movie_start_time[i]).total_seconds() for i in range(0, nmovies)])
 
-# Clean for negative values of time differences
-positive_duration = time_difference > 0
-td = time_difference[positive_duration]
-md = movie_durations[positive_duration]
-
 # Save the time information
+f = os.path.join(save_directory, 'movie_durations')
+np.save(f, movie_durations)
+
+f = os.path.join(save_directory, 'movie_mid_point')
+np.save(f, movie_mid_point)
+
+f = os.path.join(save_directory, 'time_difference')
+np.save(f, time_difference)
+
+f = os.path.join(save_directory, 'request_time.pkl')
+pickle.dump(request_time, open(f, 'wb'))
+
+f = os.path.join(save_directory, 'movie_start_time.pkl')
+pickle.dump(movie_start_time, open(f, 'wb'))
+
+f = os.path.join(save_directory, 'movie_end_time.pkl')
+pickle.dump(movie_end_time, open(f, 'wb'))
 
 
 # Analyze the sourceID column.  Split it up, find the unique elements,
 # and create a data frame.
+all_sources = []
+for source_id in df.DataSourceID.tolist():
+    ids = source_id.split(',')
+    for id in ids:
+        if id not in all_sources:
+            all_sources.append(id)
 
+
+def extract_data_source_names(a, removal_type='all'):
+    if removal_type == 'all':
+        b = a.replace(" ", "")
+        b = b.split(',')
+    if removal_type == 'lr':
+        b = a.strip()
+        b = b.split(' , ')
+    return b
+
+all_data_source_names = []
+for i, data_source_names in enumerate(df.DataSourceNames.tolist()):
+    if isinstance(data_source_names, str):
+        dsns = extract_data_source_names(data_source_names, removal_type='lr')
+        for dsn in dsns:
+            if dsn not in all_data_source_names:
+                all_data_source_names.append(dsn)
+
+
+df_new = pd.DataFrame(index=df.index, columns=all_sources)
 
 # Save the source ID information
+for this_index in df.index:
+    source_id = df.loc[this_index, "DataSourceID"]
+    ids = source_id.split(',')
+    for id in ids:
+        df_new.loc[this_index, id] = 1
+    else:
+        df_new.loc[this_index, id] = 0
+
+# Save the source ID information
+f = os.path.join(save_directory, 'source_ids.csv')
+df_new.to_csv(f)
+
+# Save the data source names
+f = os.path.join(save_directory, 'data_source_names.pkl')
+pickle.dump(all_data_source_names, open(f, 'wb'))
